274ede2a2cc4b173dbff7606ee8f4b8683a13134
Thomas905 <thomasclarisse03@gmail.com>
2022-02-03 22:41:57 +0100

    dashboard

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Prestation;
+use App\Repository\EtablisementRepository;
+use App\Repository\PrestationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,11 +17,19 @@ class HomeController extends AbstractController
      * @Route("/", name="home")
      * @IsGranted("ROLE_USER")
      */
-    public function index(): Response
+    public function index(PrestationRepository $prestationRepository, EtablisementRepository $etablisementRepository): Response
     {
         $user = $this->getUser();
+        $prestations = $prestationRepository->findAll();
+        $countprestation = count($prestations);
+
+        $etablissement = $etablisementRepository->findAll();
+        $countetablissement = count($etablissement);
+
         return $this->render('home/index.html.twig', [
             'user' => $user,
+            'countprestation' => $countprestation,
+            'countetablissement' => $countetablissement
         ]);
     }
 }
